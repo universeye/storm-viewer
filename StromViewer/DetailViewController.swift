64d1c64c17a3144e803b731b0e6b5e790f2528f3
Terry <terrykuo501@gmail.com>
2021-07-02 13:48:28 +0800

    add read me sources

--- StromViewer/DetailViewController.swift
+++ StromViewer/DetailViewController.swift
@@ -14,10 +14,13 @@ class DetailViewController: UIViewController {
     var allpictures: Int?
     var selectedInt: Int?
     
+    lazy var shareButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Picture \(selectedInt ?? 0) of \(allpictures ?? 11)"
         navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = shareButton
         
         if let image = selectedImage {
             
@@ -36,6 +39,42 @@ class DetailViewController: UIViewController {
     }
     
 
+    @objc private func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No inage found")
+            return
+        }
+        
+        guard let imageTitle = selectedImage else {
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [imageTitle, image], applicationActivities: [])
+        
+        vc.completionWithItemsHandler = {(activityType: UIActivity.ActivityType?, completed: Bool, returnedItems: [Any]?, error: Error?) in
+            
+                if error != nil {
+                    print("failed")
+                    return
+                }
+                if completed {
+                    print("success")
+                    self.showAlert()
+                }
+            }
+        
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem //only affects on ipad
+        present(vc, animated: true, completion: nil)
+    }
+    
+    
+    func showAlert() {
+        let ac = UIAlertController(title: "Success!", message: "Success", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default, handler: {_ in}))
+        
+        present(ac, animated: true, completion: nil)
+    }
+    
     
 
 }
